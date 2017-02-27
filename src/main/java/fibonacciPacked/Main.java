c86c571180ce5ae47241689d9789e08eb7352044
unknown <arekpl28@wp.pl>
2017-02-27 21:02:02 +0100

    Added gitignor

--- src/main/java/fibonacciPacked/Main.java
+++ src/main/java/fibonacciPacked/Main.java
@@ -3,10 +3,10 @@ package fibonacciPacked;
 public class Main {
     public static void main(String[] args) {
         Fibonacci fibonacciRecursive = FibonacciFabric.produce(FibonacciEnum.RECURSIVE);
-        System.out.println(fibonacciRecursive.getN(6));
+        System.out.println(fibonacciRecursive.getN(7));
         System.out.println();
         Fibonacci fibonacciIterational = FibonacciFabric.produce(FibonacciEnum.INTERATION);
-        System.out.println(fibonacciIterational.getN(4));
+        System.out.println(fibonacciIterational.getN(2));
 
     }
 }
